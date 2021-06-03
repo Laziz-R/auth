aa326a681bfb9b1f6e0742ad9c8aaf407ee3a76f
R-Laziz <rohataliyevlaziz@gmail.com>
2021-06-03 17:40:57 +0500

    da

--- auth/src/main/java/com/intern/auth/MainVerticle.java
+++ auth/src/main/java/com/intern/auth/MainVerticle.java
@@ -1,10 +1,15 @@
 package com.intern.auth;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpServerOptions;
 
 public class MainVerticle extends AbstractVerticle{
     @Override
     public void start() throws Exception {
-        // Router router = Router.Router(vertx);
+        vertx.createHttpServer(new HttpServerOptions().setPort(8090).setHost("localhost"))
+        .requestHandler(req->{
+            req.response().end("Hello");
+        })
+        .listen();
     }
 }
