ad7a028ceb8beb1e2e71f89153d70b9c908f7599
Laziz-R <rohataliyevlaziz@gmail.com>
2021-06-06 21:57:04 +0500

    da

--- src/main/java/com/intern/auth/MainVerticle.java
+++ src/main/java/com/intern/auth/MainVerticle.java
@@ -1,15 +1,65 @@
 package com.intern.auth;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServerOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.oauth2.OAuth2Auth;
+import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.auth.oauth2.OAuth2Options;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.SessionHandler;
+import io.vertx.ext.web.sstore.SessionStore;
 
-public class MainVerticle extends AbstractVerticle{
+public class MainVerticle extends AbstractVerticle {
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer(new HttpServerOptions().setPort(8090).setHost("localhost"))
-        .requestHandler(req->{
-            req.response().end("Hello");
-        })
-        .listen();
+      JsonObject github = config().getJsonObject("auth").getJsonObject("github");
+      JsonObject keycloak = config().getJsonObject("auth").getJsonObject("keycloak");
+      OAuth2Auth oauth2 = OAuth2Auth.create(vertx,
+              new OAuth2Options()
+                  .setFlow(OAuth2FlowType.AUTH_CODE)
+                  .setClientID(github.getString("clientId"))
+                  .setClientSecret(github.getString("clientSecret"))
+                  .setSite(github.getString("site"))
+                  .setTokenPath(github.getString("tokenPath"))
+                  .setAuthorizationPath(github.getString("authorizationPath")));
+
+        Router router = Router.router(vertx);
+        router.route().handler(SessionHandler.create(SessionStore.create(vertx)));
+        router.route("/github/*").handler(ctx->{
+          if(ctx.user()!=null && !ctx.user().expired()){
+            ctx.next();
+          }
+          String authorization_uri = oauth2.authorizeURL(new JsonObject()
+            .put("scope", github.getString("scope"))
+            .put("state", ctx.normalizedPath()));
+          ctx.redirect(authorization_uri);
+        });
+
+        router.get("/callback").handler(ctx->{
+          String code = ctx.request().getParam("code");
+          String state = ctx.request().getParam("state");
+          
+          oauth2.authenticate(new JsonObject()
+          .put("code", code))
+          .onSuccess(user->{
+            System.err.println("code: " + code);
+            ctx.setUser(user);
+            ctx.redirect(state);
+          })
+          .onFailure(ar->{
+            ctx.response().end("Failed");
+          });
+        });
+
+        router.get("/github/hi")
+        .handler(ctx->{
+          oauth2.userInfo(ctx.user())
+          .onSuccess(user->{
+            ctx.response().end(user.toString());
+          });
+        });
+
+        vertx.createHttpServer().requestHandler(router)
+                .listen(config().getInteger("port"), config().getString("host"));
     }
 }
---
+++ src/main/java/com/intern/auth/StartVerticle.java
@@ -0,0 +1,27 @@
+package com.intern.auth;
+
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.json.JsonObject;
+
+public class StartVerticle extends AbstractVerticle{
+    @Override
+    public void start() throws Exception {
+        String path = System.getProperty("user.dir") + "/src/main/resources/config.yaml";
+        ConfigStoreOptions yamlStore = new ConfigStoreOptions()
+        .setType("file")
+        .setFormat("yaml")
+        .setConfig(new JsonObject()
+          .put("path", path)
+        );
+        ConfigRetrieverOptions options = new ConfigRetrieverOptions().addStore(yamlStore);
+        ConfigRetriever retriever = ConfigRetriever.create(vertx, options);
+        retriever.getConfig()
+        .onSuccess(conf->{
+          vertx.deployVerticle(new MainVerticle(), new DeploymentOptions().setConfig(conf));
+        });
+    }
+}
