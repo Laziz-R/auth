0a5ecd0d9a2b50fe4f023c78cbe44d3957c7da00
Laziz-R <rohataliyevlaziz@gmail.com>
2021-06-19 09:19:09 +0500

    da

--- src/main/java/com/intern/auth/KeyDemoVert.java
+++ src/main/java/com/intern/auth/KeyDemoVert.java
@@ -2,8 +2,12 @@ package com.intern.auth;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.authorization.AuthorizationProvider;
+import io.vertx.ext.auth.authorization.RoleBasedAuthorization;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
+import io.vertx.ext.auth.oauth2.authorization.KeycloakAuthorization;
+import io.vertx.ext.auth.oauth2.providers.GithubAuth;
 import io.vertx.ext.auth.oauth2.providers.KeycloakAuth;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.OAuth2AuthHandler;
@@ -14,53 +18,67 @@ public class KeyDemoVert extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         JsonObject keyJson = config().getJsonObject("auth").getJsonObject("keycloak");
+        JsonObject gitJson = config().getJsonObject("auth").getJsonObject("github");
         String host = config().getString("host");
         int port = config().getInteger("port");
-
+        String baseURL = "http://"+host+":"+port;
         Router router = Router.router(vertx);
         router.route().handler(SessionHandler.create(LocalSessionStore.create(vertx)));
 
         OAuth2Auth keyAuth = KeycloakAuth.create(vertx, OAuth2FlowType.AUTH_CODE, keyJson);
-        OAuth2AuthHandler authHandler = OAuth2AuthHandler.create(vertx, keyAuth).setupCallback(router.route("/callback"));
-        router.route("/key2/*").handler(authHandler);
-        router.get("/key2/hi").handler(ctx->ctx.end("Hiiiiiii"));
-        router.get("/key2/user").handler(ctx->{
+        OAuth2Auth gitAuth = GithubAuth.create(vertx, gitJson.getString("clientId"), gitJson.getString("clientSecret"));
+        OAuth2AuthHandler gitAuthHandler = OAuth2AuthHandler.create(vertx, gitAuth, baseURL+"/callbac").setupCallback(router.route("/callbac")).withScope("user");
+        OAuth2AuthHandler keyAuthHandler = OAuth2AuthHandler.create(vertx, keyAuth, baseURL+"/callback").setupCallback(router.route("/callback"));
+        
+        router.route("/git/*").handler(gitAuthHandler);
+        router.get("/git/hi").handler(ctx->ctx.end("Hi from Github!"));
+        router.get("/git/user").handler(ctx->{
+            gitAuth.userInfo(ctx.user())
+                .onSuccess(user->ctx.end(user.toString()))
+                .onFailure(ar->ctx.end(ar.getMessage()));
+        });
+        router.get("/git/logout").handler(ctx->{
+            gitAuth.revoke(ctx.user())
+                .onSuccess(v->System.out.print("Succes"))
+                .onFailure(System.out::println);
+            ctx.redirect(gitAuth.endSessionURL(ctx.user(), new JsonObject().put("redirect_uri", baseURL+"/hi")));
+            ctx.session().destroy();
+        });
+
+        router.route("/key/*").handler(keyAuthHandler);
+        router.get("/key/hi").handler(ctx->ctx.end("Hi, from Keycloak!"));
+        router.get("/key/admin").handler(ctx->{
+            AuthorizationProvider authz = KeycloakAuthorization.create();
+            authz.getAuthorizations(ctx.user())
+            .onSuccess(ar->{
+                if(RoleBasedAuthorization.create("admin")
+                    .setResource("vertx")
+                    .match(ctx.user())){
+                        ctx.end("<h1>Hi, boss!</h1>");
+                } else{
+                    ctx.reroute("/hi");
+                }
+            })
+            .onFailure(ar->ctx.redirect(baseURL + "/hi?msg=" + ar.getMessage()));
+        });
+        router.get("/key/user").handler(ctx->{
             keyAuth.userInfo(ctx.user())
                 .onSuccess(user->ctx.end(user.toString()))
                 .onFailure(ar->ctx.end(ar.getMessage()));
         });
-
-        router.get("/hi").handler(ctx->ctx.end("<h1>Hi, buddy!</h1>"));
-
-        router.get("/key/*").handler(ctx->{
-            if(ctx.user()==null){
-                ctx.session().put("back", ctx.request().absoluteURI());
-                String auth_url = keyAuth.authorizeURL(new JsonObject()
-                    .put("redirect_uri", "http://"+host+":"+port+"/callback")
-                    .put("scope", "profile"));
-                ctx.redirect(auth_url);
-            } else{
-                if(ctx.user().expired()){
-                    keyAuth.refresh(ctx.user())
-                        .onSuccess(user->ctx.setUser(user));
-                }
-                ctx.next();
-            }
+        router.get("/key/logout").handler(ctx->{
+            ctx.redirect(keyAuth.endSessionURL(ctx.user(), new JsonObject().put("redirect_uri", baseURL+"/hi")));
+            ctx.session().destroy();
         });
-        router.get("/key/hi").handler(ctx->ctx.end("<h1>Hi, boss!</h1>"));
 
-        router.get("/callback").handler(ctx->{
-            JsonObject tokenConfig = new JsonObject()
-                .put("code", ctx.request().getParam("code")) 
-                .put("redirect_uri", ctx.session().get("back"));
-            keyAuth.authenticate(tokenConfig)
-                .onComplete(ar->{
-                    if(ar.succeeded()){
-                        ctx.setUser(ar.result());
-                    } else{
-                        ctx.end(ar.cause().getMessage() + "\n" + ctx.session().get("back"));
-                    }
-                });
+        router.get("/hi").handler(ctx->{
+            String msg = ctx.request().getParam("msg");
+            ctx.response().setChunked(true);
+            ctx.response().write("<h1>Hi, buddy!</h1>");
+            if(msg!=null){
+                ctx.response().write("Message: " + msg);
+            }
+            ctx.end();
         });
         vertx.createHttpServer().requestHandler(router).listen(port, host);
     }
